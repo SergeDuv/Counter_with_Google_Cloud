4c502e66dd79e57bffdb83bac9c6f7782c55858d
SergeDuv <100307642+SergeDuv@users.noreply.github.com>
2022-03-07 10:12:57 -0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# id чата в телеграме куда будут отправляться ошибки. чтобы узнать свой id - напишите боту @userinfobot 
+tg_chat_id = 'XXXXXXX' 
+# токен бота. чтобы его получить, создайте бота написав @BotFather
+tg_bot_token = 'YYYYYYY:YYYYYYYYYYYYYYYYYYYYYYYYYY' 
+# название таблицы в BigQuery куда будут писаться события. Ее нужно создать заранее.
+bq_table_name = 'project-name.dataset.powerbi_views'
